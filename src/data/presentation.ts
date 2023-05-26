6f44359601ca9e92bc8ac0a660d3e3334231cb86
MaevaWolff <maeva.wolff@hetic.net>
2023-05-26 15:50:45 +0200

    feat: add socials on config file

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,7 +1,13 @@
+type Social = {
+  label: string;
+  link: string;
+};
+
 type Presentation = {
   mail: string;
   title: string;
   description: string;
+  socials: Social[];
 };
 
 const presentation: Presentation = {
@@ -9,6 +15,20 @@ const presentation: Presentation = {
   title: "Hi, I’m Maxence 👋",
   description:
     "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
+  socials: [
+    {
+      label: "Twiiter",
+      link: "https://twitter.com/itsstormzz_",
+    },
+    {
+      label: "Bento",
+      link: "https://bento.me/m-wolff",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/MaeWolff",
+    },
+  ],
 };
 
 export default presentation;
