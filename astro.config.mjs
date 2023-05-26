4b5c714df87f833a8ccbf26e5c55be05be284636
MaevaWolff <maeva.wolff@hetic.net>
2023-05-27 00:42:07 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
