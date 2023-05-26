da8481e18683a0c24c05baefff72e5246e1e4831
MaevaWolff <maeva.wolff@hetic.net>
2023-05-26 12:39:54 +0200

    chore: prepare astro:content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "nord",
+      wrap: false,
+    },
+  },
 });
