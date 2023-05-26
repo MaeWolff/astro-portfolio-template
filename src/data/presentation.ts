92698a3e9e3568fce02af67e487d1b33127564f8
MaevaWolff <maeva.wolff@hetic.net>
2023-05-26 14:48:07 +0200

    feat: add email on presentation data

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,9 +1,11 @@
 type Presentation = {
+  mail: string;
   title: string;
   description: string;
 };
 
 const presentation: Presentation = {
+  mail: "maxencewolff.pro@gmail.com",
   title: "Hi, I’m Maxence 👋",
   description:
     "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
