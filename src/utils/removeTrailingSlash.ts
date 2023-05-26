16a98814eb5cc2746961c4273654d12837f47032
MaevaWolff <maeva.wolff@hetic.net>
2023-05-26 18:13:32 +0200

    refactor: remove useless code

--- src/utils/removeTrailingSlash.ts
+++
@@ -1,5 +0,0 @@
-export default function removeTrailingSlash(pathname: string) {
-  const matchTrailingSlash = /\w+\/$/;
-  if (matchTrailingSlash.test(pathname)) return pathname.slice(0, -1);
-  return pathname;
-}
