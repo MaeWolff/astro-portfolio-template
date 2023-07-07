fc12ee5714fb092f98aa9f6b5654813f1730812b
Course Rye <1430657328@qq.com>
2023-07-07 15:17:18 +0800

    Update formatDate.ts

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,6 @@
 export default function formatDate(date: Date) {
-  return new Intl.DateTimeFormat('zh-CN', { dateStyle: 'short', timeZone: 'Asia/Shanghai' }).format(date));
+  return new Intl.DateTimeFormat('zh-CN', { 
+    dateStyle: 'short',
+    timeZone: 'Asia/Shanghai'
+  }).format(date) ;
 }
