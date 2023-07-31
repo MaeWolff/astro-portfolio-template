9d879256fefc335245683c19c627933f39b18622
Khush Chopra <43996455+khushChopra@users.noreply.github.com>
2023-07-31 22:18:50 +0530

    Adding provision to add profile photo with responsive layout (#6)

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -8,11 +8,13 @@ type Presentation = {
   title: string;
   description: string;
   socials: Social[];
+  profile?: string;
 };
 
 const presentation: Presentation = {
   mail: "maxencewolff.pro@gmail.com",
   title: "Hi, I’m Maxence 👋",
+  profile: "/profile.webp",
   description:
     "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
   socials: [
