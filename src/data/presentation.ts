e2e7ebb610a46e9a90dceb387fca48a21ee92e7c
MaevaWolff <maeva.wolff@hetic.net>
2023-08-01 05:56:36 +0200

    refactor: remove profile picture for the default example

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,7 +14,7 @@ type Presentation = {
 const presentation: Presentation = {
   mail: "maxencewolff.pro@gmail.com",
   title: "Hi, I’m Maxence 👋",
-  profile: "/profile.webp",
+  // profile: "/profile.webp",
   description:
     "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
   socials: [
