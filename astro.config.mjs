5d560d61c923c19fd8ded5d76ae209e7c3ae8b09
Maxence <56693082+MaeWolff@users.noreply.github.com>
2023-09-18 17:31:57 +0200

    fea:t add configuration to generate robot.txt file (#14)
    * chore: install astro-robots-txt package
    
    * chore: add robotsTxt() in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
